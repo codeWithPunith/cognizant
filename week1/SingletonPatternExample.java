2093b5de0b22972aa12916dca6c02edf3f213082
codeWithPunith <codewithpunith@gmail.com>
2026-06-19 23:59:06 +0530

    Week 1 first assignment

---
+++ week1/SingletonPatternExample.java
@@ -0,0 +1,21 @@
+package solution.week1;
+
+public class SingletonPatternExample {
+    static class Logger{
+        static Logger login = new Logger();
+        String msg="";
+        private Logger(){
+             msg = "Logging user 1";
+        }
+        public static Logger getLogin(){
+            return login;
+        }
+    }
+    public static void main(String[] args){
+          Logger log1 = Logger.getLogin();
+          Logger log2 = Logger.getLogin();
+          System.out.println(log1.msg);
+          System.out.println(log2.msg);
+    }   
+
+}
