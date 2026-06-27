47d6388c8db223251175a66311dcb2e4dd0ba930
codeWithPunith <codewithpunith@gmail.com>
2026-06-27 14:45:19 +0530

    Junit Exercise 5

---
+++ week1/Junit/JunitEx5/junitdemo/src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Teardown");
+    }
+
+    @Test
+    void testAddition() {
+
+       
+        int a = 10;
+        int b = 20;
+
+      
+        int result = calculator.add(a, b);
+
+        
+        assertEquals(30, result);
+    }
+}
