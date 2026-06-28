51ad894f820ce430cb5da84e85920c11101f53ca
codeWithPunith <codewithpunith@gmail.com>
2026-06-28 19:45:23 +0530

    Exercise 1

---
+++ week2/exercise1/LibraryManagement/src/main/java/com/library/services/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("BookService bean created");
+    }
+
+    public void display() {
+        System.out.println("Inside BookService");
+    }
+}
