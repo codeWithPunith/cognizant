d15dbcf8fce5037c01009ea538b8a0ad2d355723
codeWithPunith <codewithpunith@gmail.com>
2026-06-28 19:52:41 +0530

    completed week 2

---
+++ week2/exercise2/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,19 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void display() {
+        System.out.println("Inside BookService");
+        if (bookRepository != null) {
+            bookRepository.display();
+        }
+    }
+}
